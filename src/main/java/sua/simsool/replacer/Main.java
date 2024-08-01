acece0d0c1a04668f7fe664d8f5d7ee7da8bdb5f
SimSool <79349144+SILENCE-SIMSOOL@users.noreply.github.com>
2024-08-01 18:14:14 +0900

    Update Main.java

--- src/main/java/sua/simsool/replacer/Main.java
+++ src/main/java/sua/simsool/replacer/Main.java
@@ -3,10 +3,10 @@ package sua.simsool.replacer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "suareplacer", version = "1.2", useMetadata=true)
+@Mod(modid = "suareplacer", version = "2.1", useMetadata=true)
 public class Main {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
-		System.out.println("[Silence]: Loaded Silence Utils Addon (Replacer) - v1.0");
+		System.out.println("[Silence]: Loaded Silence Utils Addon (Replacer) - v2.1");
 	}
 }
