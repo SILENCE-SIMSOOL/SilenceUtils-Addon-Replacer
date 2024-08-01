d0d80278397a4c215da40872f726077d28be3174
SimSool <79349144+SILENCE-SIMSOOL@users.noreply.github.com>
2024-08-01 20:15:10 +0900

    Update Main.java

--- src/main/java/sua/simsool/replacer/Main.java
+++ src/main/java/sua/simsool/replacer/Main.java
@@ -2,11 +2,17 @@ package sua.simsool.replacer;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import silence.simsool.config.Config;
+import silence.simsool.manager.MiningManager;
 
 @Mod(modid = "suareplacer", version = "2.1", useMetadata=true)
 public class Main {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		System.out.println("[Silence]: Loaded Silence Utils Addon (Replacer) - v2.1");
+	}
+	public static boolean isEnable() {
+		if (Config.Replacer && MiningManager.isSkyblockMining() && MiningManager.inMiningIsland()) return true;
+		return false;
 	}
 }
